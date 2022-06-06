871864f6327b2755ba71604705e2c7a51dc95cff
David <david.sierra@packetfabric.com>
2022-06-06 19:22:12 -0400

    Solar page displaying - missing result though

--- 6-async-quart/program/routes.py
+++ 6-async-quart/program/routes.py
@@ -1,6 +1,5 @@
 import aiohttp
 import asyncio
-import requests
 from program import app
 from quart import request
 from quart import render_template
@@ -16,7 +15,22 @@ async def get_apod():
             data = await resp.json()
     return data
 
+
+async def get_solar_data(solar_bodytype):
+    url = f"https://api.le-systeme-solaire.net/rest/bodies/?filter=bodyType,eq,{solar_bodytype.lower()}"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            resp.raise_for_status
+            data = await resp.json()
+    bodies = []
+    for l in data.values():
+        for d in l:
+            bodies.append(d.get("englishName"))
+    return data
+
+
 apod = asyncio.run(get_apod())
+
 
 @app.route('/')
 @app.route('/index')
@@ -25,25 +39,13 @@ async def index():
     return await render_template('index.html', time=timenow, apod=apod)
 
 
-#@app.route('/solar', methods=['GET', 'POST'])
-#async def solarSystem():
-#    bodies = []
-#    solar_bodytype = ""
-#    if request.method == 'POST' and 'bodytype' in request.form:
-#        solar_bodytype = request.form.get('bodytype')
-#        bodies = get_solar_data(solar_bodytype)
-#    return render_template('solar.html', bodies=bodies,
-#                            solar_bodytype=solar_bodytype.capitalize())
-#
-#
-#async def get_solar_data(solar_bodytype):
-#    url = f"https://api.le-systeme-solaire.net/rest/bodies/?filter=bodyType,eq,{solar_bodytype.lower()}"
-#    async with aiohttp.ClientSession as session:
-#        async with session.get(url) as resp:
-#            resp.raise_for_status
-#            data = await resp.json()
-#    bodies = []
-#    for l in data.values():
-#        for d in l:
-#            bodies.append(d.get("englishName"))
-#    return bodies
+@app.route('/solar', methods=['GET', 'POST'])
+async def solarSystem():
+    bodies = []
+    solar_bodytype = ""
+    form = await request.form
+    if request.method == 'POST' and 'bodytype' in form:
+        solar_bodytype = form.get('bodytype')
+        bodies = await get_solar_data(solar_bodytype)
+    return await render_template('solar.html', bodies=bodies,
+                                solar_bodytype=solar_bodytype.capitalize())
