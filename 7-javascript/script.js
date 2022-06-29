5a7a2819f86f018f63e3a10ca7077dab982dc54e
vadid.eisrra <david.sierra@packetfabric.com>
2022-06-29 07:30:03 -0400

    Changes: - External JS file - Event listener working with radio buttons

---
+++ 7-javascript/script.js
@@ -0,0 +1,95 @@
+window.onload = function() {
+  let billTotal = document.getElementById("billAmount");
+  let tipPer = document.getElementById("totalTip");
+  let totalPer = document.getElementById("totalPerson");
+
+  document.getElementById("selectTip").addEventListener("click", function(e) {
+    if(e.target.value) {
+      let tipPercent = +document.querySelector('input[name="percent"]:checked').value;
+      let peeps = +document.getElementById("numPeople").value;
+      if (peeps == 0){
+        peeps = 1; 
+      };
+      let bill = +billTotal.value;
+      let tip = (tipPercent / 100) * bill;
+      let perPersonTip = tip / peeps;
+      let perPersonTotal = (bill + tip) / peeps;
+
+      tipPer.textContent =  "$" + Math.round(perPersonTip * 100) / 100;
+      totalPer.textContent = "$" + Math.round(perPersonTotal * 100) / 100;
+      document.getElementById("resetButton").style.opacity = "100";
+    };
+  });
+
+  document.getElementById("customTip").addEventListener("click", function(e) {
+    if(e.target.value) {
+      tipPercent = parseInt(e.target.value);
+      let peeps = +document.getElementById("numPeople").value;
+      if (peeps == 0){
+        peeps = 1; 
+      };
+      let bill = +billTotal.value;
+      let tip = (tipPercent / 100) * bill;
+      let perPersonTip = tip / peeps;
+      let perPersonTotal = (bill + tip) / peeps;
+
+      tipPer.textContent =  "$" + Math.round(perPersonTip * 100) / 100;
+      totalPer.textContent = "$" + Math.round(perPersonTotal * 100) / 100;
+      document.getElementById("resetButton").style.opacity = "100";
+    };
+  });
+
+  document.getElementById("customTip").addEventListener("input", function(e) {
+    if(e.target.value) {
+      tipPercent = parseInt(e.target.value);
+      let peeps = +document.getElementById("numPeople").value;
+      if (peeps == 0){
+        peeps = 1; 
+      };
+      let bill = +billTotal.value;
+      let tip = (tipPercent / 100) * bill;
+      let perPersonTip = tip / peeps;
+      let perPersonTotal = (bill + tip) / peeps;
+
+      tipPer.textContent =  "$" + Math.round(perPersonTip * 100) / 100;
+      totalPer.textContent = "$" + Math.round(perPersonTotal * 100) / 100;
+      document.getElementById("resetButton").style.opacity = "100";
+    };
+  });
+  document.getElementById("numPeople").addEventListener("click", function(e) {
+    if(e.target.value) {
+      let tipPercent = +document.querySelector('input[name="percent"]:checked').value;
+      let peeps = +document.getElementById("numPeople").value;
+      if (peeps == 0){
+        peeps = 1; 
+      };
+      let bill = +billTotal.value;
+      let tip = (tipPercent / 100) * bill;
+      let perPersonTip = tip / peeps;
+      let perPersonTotal = (bill + tip) / peeps;
+
+      tipPer.textContent =  "$" + Math.round(perPersonTip * 100) / 100;
+      totalPer.textContent = "$" + Math.round(perPersonTotal * 100) / 100;
+      document.getElementById("resetButton").style.opacity = "100";
+    };
+  });
+
+  document.getElementById("numPeople").addEventListener("input", function(e) {
+    if(e.target.value) {
+      let tipPercent = +document.querySelector('input[name="percent"]:checked').value;
+      let peeps = +document.getElementById("numPeople").value;
+      if (peeps == 0){
+        peeps = 1; 
+      };
+      let bill = +billTotal.value;
+      let tip = (tipPercent / 100) * bill;
+      let perPersonTip = tip / peeps;
+      let perPersonTotal = (bill + tip) / peeps;
+
+      tipPer.textContent =  "$" + Math.round(perPersonTip * 100) / 100;
+      totalPer.textContent = "$" + Math.round(perPersonTotal * 100) / 100;
+      document.getElementById("resetButton").style.opacity = "100";
+    };
+  });
+
+};
