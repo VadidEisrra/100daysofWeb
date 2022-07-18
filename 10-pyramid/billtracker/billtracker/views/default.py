6dcaa2dc4c38e3763bab0dd28b8101749a57747f
vadid.eisrra <david.sierra@packetfabric.com>
2022-07-18 17:13:34 -0400

    Presenting data on page

--- 10-pyramid/billtracker/billtracker/views/default.py
+++ 10-pyramid/billtracker/billtracker/views/default.py
@@ -1,6 +1,15 @@
+from pyramid.request import Request
 from pyramid.view import view_config
+
+from billtracker.data import repository
 
 
 @view_config(route_name='home', renderer='billtracker:templates/home/default.pt')
-def my_view(request):
-    return {'project': 'Bill Tracker Pro'}
+def my_view(_: Request):
+    user_id = 1 # probably get from a cookie
+
+    user = repository.get_user_by_id(user_id)
+    return {
+        'user': user,
+    }
+
