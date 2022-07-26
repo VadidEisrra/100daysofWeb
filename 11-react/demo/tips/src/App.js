d77ce52956fef069070b233dbfff7d682df75a8f
vadid.eisrra <david.sierra@packetfabric.com>
2022-07-26 16:28:27 -0400

    Working to integrate api to frontend

--- 11-react/demo/tips/src/App.js
+++ 11-react/demo/tips/src/App.js
@@ -1,8 +1,13 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const axios = require('axios')
+
 const PAGE_TITLE = `PyBites Python Tips API`;
 const TWITTER_ICON = 'https://codechalleng.es/static/img/icon-twitter.9f5315ee958c.png'
+const TIPS_ENDPOINT = 'http://127.0.0.1:8000/api/'
+
 function Tip(props) {
     return(
       <div className="tip">
@@ -13,35 +18,73 @@ function Tip(props) {
         </p>
         <pre>{props.code}</pre>
         { props.share_link &&
-	  <p>
-	    <a href={props.share_link} target="_blank">
-	      <img src={TWITTER_ICON} alt="share"/>
-	    </a>
-	  </p>
-	}
+        <p>
+          <a href={props.share_link} target="_blank">
+            <img src={TWITTER_ICON} alt="share"/>
+          </a>
+        </p>
+      }
       </div>
     )
 }
 
-function App() {
-  return (
-    <div className="App">
-      <h2>{PAGE_TITLE}</h2>
-  
-      <form id="searchTips">
-	<input type="text"
-	  placeholder="filter tips"
-	  value=''
-	  onChange='' />
-      </form>
+class App extends Component {
 
-      <div id="tips">
-	<Tip tip="123" code="print1" link="http" />
-	<Tip tip="456" code="print1"  />
-	<Tip tip="789" code="print1" share_link="http" />
+  constructor(props){
+    super(props);
+    this.state= {
+      orgTips: [],
+      showTips: [],
+      filterStr: '',
+    }
+    this.onFilterChange = this.onFilterChange.bind(this);
+    this.filterTips= this.filterTips.bind(this);
+  }
+
+  componentDidAmount(){
+    // call api using axios
+    axios.get(TIPS_ENDPOINT)
+    .then(response => {
+      this.setState({
+        orgTips: response.data,
+        showTips: response.data
+      })
+    })
+    .catch(function(error){
+      console.log(error);
+    })
+  }
+
+  onFilterChange(event){
+    // filter orgTips into showTips
+    console.log('onFilterChange called');
+  }
+
+  filterTips = (filterStr) => {
+    // helper for aonFilterChange
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h2>{PAGE_TITLE}</h2>
+    
+        <form id="searchTips">
+          <input type="text"
+            placeholder="filter tips"
+            value={this.state.filterStr}
+            onChange={this.onFilterChange} />
+        </form>
+  
+        <div id="tips">
+          {this.state.showTips.map((tip, index) =>
+            <Tip {...tip} key={index} filterStr={this.state.filterStr} />
+          )}
+        </div>
+
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
