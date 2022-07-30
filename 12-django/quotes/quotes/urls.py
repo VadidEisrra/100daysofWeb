9507629bdea70d2f624655b2458ecde94b4c41c1
vadid.eisrra <david.sierra@packetfabric.com>
2022-07-30 07:32:17 -0400

    First working template/view!

--- 12-django/quotes/quotes/urls.py
+++ 12-django/quotes/quotes/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'quotes'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.quote_list, name='quote_list'),
+    path('<int:pk>', views.quote_detail, name='quote_detail'),
+    path('new', views.quote_new, name='quote_new'),
+    path('edit/<int:pk>', views.quote_edit, name='quote_edit'),
+    path('delete/<int:pk>', views.quote_delete, name='quote_delete'),
 ]
--- 12-django/quotes/quotes/views.py
+++ 12-django/quotes/quotes/views.py
@@ -1,4 +1,28 @@
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse('Welceme to Django')
+
+from .models import Quote
+from .forms import QuoteForm
+
+
+def quote_list(request):
+    quotes = Quote.objects.all()
+    return render(request, 'quotes/quote_list.html', {'quotes': quotes})
+
+
+def quote_detail(request, pk):
+    pass
+
+
+def quote_new(request):
+    pass
+
+
+def quote_edit(request, pk):
+    pass
+
+
+def quote_delete(request, pk):
+    pass
